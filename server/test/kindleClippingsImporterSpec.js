0f57bfad0dd7aa45320d6c9b01ab79d509718719
Juri <juri.strumpflohner@gmail.com>
2014-07-26 19:10:41 +0200

    adds first test

---
+++ server/test/kindleClippingsImporterSpec.js
@@ -0,0 +1,15 @@
+var expect = require('chai').expect;
+ 
+describe('The KindleImporter', function(){
+
+
+    describe('when calling parse', function(){
+
+
+        it('should extract a list of books', function(){
+            expect('a').to.equal('a');
+        });
+
+    });
+
+});
--- server/test/mocha.js
+++
@@ -1,9 +0,0 @@
-var expect = require('chai').expect;
- 
-describe('Tags', function(){
-
-    it('should work', function(){
-        expect(true).to.equal(true);
-    });
-
-});
