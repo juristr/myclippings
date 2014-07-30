c24725d2046059d10af684ef792324c5b32d9545
Juri <juri.strumpflohner@gmail.com>
2014-07-30 23:13:06 +0200

    adds express api endpoint

---
+++ server/server.js
@@ -0,0 +1,40 @@
+var http = require('http');
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+//app.use(express.favicon());
+// app.use(express.logger({
+//      format: 'tiny',
+//      stream: fs.createWriteStream('app.log', {'flags': 'w'})
+// }));
+
+app.use(express.static(__dirname + '/../client'));
+// app.use(express.urlencoded());
+app.use(bodyParser());
+app.use(methodOverride());
+// app.use(express.responseTime());
+// Add the errorHander middleware
+//app.use(express.errorHandler());
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.json({
+    message: err.message,
+    stack: err.stack
+  })
+});
+
+
+app.get('/ping', function(req, res){
+  res.send('pong');
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
